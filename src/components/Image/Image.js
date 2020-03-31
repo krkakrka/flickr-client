a5cd75713e96df777965cdef140a66b87fe07699
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2020-03-31 16:46:48 +0300

    Pretty details on hover

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,11 +3,20 @@ import styles from './Image.module.css';
 
 function Image({ flickrImage, className = '' }) {
   const { title, owner, img } = flickrImage;
+
   return (
     <div className={`${styles.container} ${className}`}>
-      {/* <div>{title}</div>
-      <div>{owner}</div> */}
-      <img className={styles.image} src={img} alt="flickr" />
+      <div className={styles.container__details}>
+        <div>{title || '-'}</div>
+        <hr className={styles.container__divider} />
+        <div>{owner || '-'}</div>
+        <button className={styles.container__favourite}>Favourite</button>
+      </div>
+      <img
+        className={styles.container__image}
+        src={img}
+        alt="flickr"
+      />
     </div>
   );
 }
